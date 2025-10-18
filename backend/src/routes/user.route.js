f3dca9851994eed616ed7c92c670b93e3f176188
Võ Chí Lâm <lamcity4869@gmail.com>
2025-10-18 19:37:28 +0700

    feat: upload avatar

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -4,7 +4,9 @@ import loginUserController from "../controllers/userControllers/loginUser.contro
 import logoutUserController from "../controllers/userControllers/logoutUser.controller.js";
 import searchProductByNameController from "../controllers/sharedControllers/searchProductByName.controller.js";
 import getAllProductsController from "../controllers/sharedControllers/getAllProducts.controller.js";
+import uploadAvatarController from "../controllers/sharedControllers/uploadAvatar.controller.js";
 import verifyToken from "../middlewares/verifyToken.middleware.js";
+import {upload} from "../utils/multer.js";
 
 const router = express.Router();
 
@@ -13,5 +15,6 @@ router.post("/login", loginUserController);
 router.post("/logout", verifyToken, logoutUserController);
 router.get('/product/search', verifyToken, searchProductByNameController);
 router.get('/products', verifyToken, getAllProductsController);
+router.post('/upload', verifyToken, upload.single("avatar"), uploadAvatarController);
 
 export default router;
